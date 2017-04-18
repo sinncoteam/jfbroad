8a8f206aa7f8f39930366f03ca0566002ece2c90
sinnco <tianofxuan@163.com>
2017-04-18 21:00:23 +0800

    redpack edit commit

--- web/FH.Cms/Controllers/PrizeController.cs
+++ web/FH.Cms/Controllers/PrizeController.cs
@@ -9,6 +9,7 @@ using JFB.Systems.Domain.Service;
 using JFB.Systems.Domain.Info;
 using JFB.Business.Domain.Service;
 using JFB.Business.Domain.Model;
+using JFB.Cms.Models;
 
 namespace JFB.Cms.Controllers
 {
@@ -47,5 +48,71 @@ namespace JFB.Cms.Controllers
             return Json(ar, JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult ResetStatus(string ids ,int type)
+        {
+            AjaxMsgResult result = new AjaxMsgResult();
+            RedPackService x_rpService = new RedPackService();
+            int i = x_rpService.ResetStatus(ids, type);
+            if( i > 0)
+            {
+                result.Success = true;
+                result.Msg = "重置状态成功";
+            }
+            else
+            {
+                result.Success = false;
+                result.Msg = "重置状态处理失败";
+            }
+            return Json(result);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            RedPackEditDto dto = new RedPackEditDto();
+            if (id > 0)
+            {
+                RedPackService x_rpService = new RedPackService();
+                dto.RPInfo = x_rpService.GetById(id);
+
+            }
+            if (dto.RPInfo == null)
+            {
+                dto.RPInfo = new RedPack();
+            }
+            return View(dto);
+        }
+
+        public JsonResult EditSave(RedPack rp)
+        {
+            AjaxMsgResult result = new AjaxMsgResult();
+            RedPackService x_opService = new RedPackService();
+            if (rp.ID > 0)  //更新
+            {
+                int i = x_opService.Update(() => new RedPack() {
+                     RbCount = rp.RbCount,
+                      RbMoney = rp.RbMoney,
+                       RbTotal = rp.RbTotal,
+                        UpdateTime = DateTime.Now,
+                         GetPercent = rp.GetPercent
+                }, a => a.ID== rp.ID);
+                if (i > 0)
+                {
+                    result.Success = true;
+                }
+                else
+                {
+                    result.Msg = "更新失败，没有找到该用户！";
+                }
+            }
+            else
+            {
+                rp.IsValid = 1;
+                rp.CreateTime = DateTime.Now;
+                rp.UpdateTime = DateTime.Now;
+                x_opService.Insert(rp);
+                result.Success = true;
+            }
+            return Json(result);
+        }
     }
 }
--- web/JFB.Business.Domain/Service/RedPackService.cs
+++ web/JFB.Business.Domain/Service/RedPackService.cs
@@ -10,5 +10,23 @@ namespace JFB.Business.Domain.Service
 {
     public class RedPackService : Repository<RedPack, RedPack>
     {
+        /// <summary>
+        /// 重置状态
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public int ResetStatus(string ids, int type)
+        {
+            string[] arr = ids.Split(new char[1] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var s in arr)
+            {
+                Convert.ToInt32(s);
+            }
+            ids = ids.Trim(',');
+            string sql = "update t_d_redpack set isvalid = " + type + " where id in(" + ids + ")";
+            int i = DataHelper.ExcuteNonQuery(sql);
+            return i;
+        }
     }
 }
