c1b569ceb7bfccaceb312432712f8de3a36cffe6
sinnco <tianofxuan@163.com>
2017-04-20 19:59:06 +0800

    wx login commit

--- web/JFB.Business.Domain/Service/RedPackListService.cs
+++ web/JFB.Business.Domain/Service/RedPackListService.cs
@@ -16,5 +16,86 @@ namespace JFB.Business.Domain.Service
             string sql = "select u.nickname, u.openid, rp.rbname, rpl.* from t_d_redpack_list rpl inner join t_d_redpack rp on rp.ID = rpl.pack_id inner join t_d_user u on u.ID = rpl.user_id where rpl.packstatus = 0";
             return DataHelper.Fill<RedPackListInfo>(sql);
         }
+        static object locker = new object();
+        /// <summary>
+        /// 用户抽奖红包逻辑
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public RedPackListInfo CheckInRedPack(int userId)
+        {
+            RedPackListInfo info = new RedPackListInfo();
+            List<RedPackCheck> clist = new List<RedPackCheck>();
+            RedPackService x_rpService = new RedPackService();
+            var list = x_rpService.Get(a => a.IsValid == 1);
+            int total = 0;
+            foreach(var item in list)
+            {
+                int nextv = total + item.GetPercent;
+                RedPackCheck rc = new RedPackCheck()
+                {
+                    RedPackId = item.ID,
+                     MinV = total,
+                      MaxV = nextv - 1
+                };
+                total = nextv;
+            }
+            Random rnd = new Random(DateTime.Now.GetHashCode());
+            int v = rnd.Next(1, total);
+            var checkitem = clist.Where(a => a.MaxV >= v && a.MinV <= v).FirstOrDefault();
+            if( checkitem != null)
+            {
+                lock (locker)
+                {
+                    var sitem = this.Get(a => a.UserId == userId).FirstOrDefault();
+                    if (sitem == null)
+                    {
+                        var nowitem = x_rpService.Get(a => a.ID == checkitem.RedPackId && a.IsValid == 1).FirstOrDefault();
+                        if (nowitem != null)
+                        {
+                            if (nowitem.RbCount > 0)
+                            {
+                                x_rpService.Update(() => new RedPack() { RbCount = nowitem.RbCount - 1 }, a => a.ID == nowitem.ID);
+                                RedPackListInfo rpinfo = new RedPackListInfo()
+                                {
+                                    PackId = nowitem.ID,
+                                    GetTime = DateTime.Now,
+                                    PackMoney = nowitem.RbMoney,
+                                    UserId = userId,
+                                    PackStatus = 0
+                                };
+                                this.Insert(rpinfo);
+                                info.PackId = nowitem.ID;
+                                info.RbName = nowitem.RbName;
+                                info.PackMoney = nowitem.RbMoney;
+                                return info;
+                            }
+                            else
+                            {
+                                info.PackId = 0;
+                                info.RbName = "红包已发完，请留意后续活动";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        info.PackId = -1;
+                        info.RbName = "您已领取过红包";
+                    }
+                }
+            }
+            RedPackListInfo rpinfo2 = new RedPackListInfo()
+            {
+                PackId = 0,
+                GetTime = DateTime.Now,
+                PackMoney = 0,
+                UserId = userId,
+                PackStatus = 0
+            };
+            this.Insert(rpinfo2);
+            info.PackId = 0;
+            info.RbName = "很遗憾，本次未能抽中红包";
+            return info;
+        }
     }
 }
---
+++ web/JFB.Wx/Controllers/WxController.cs
@@ -0,0 +1,79 @@
+﻿using JFB.Business.Domain.Info;
+using JFB.Business.Domain.Service;
+using JFB.Wx.Component;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Senparc.Weixin.MP.AdvancedAPIs.OAuth;
+using Senparc.Weixin.MP.TenPayLibV3;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JFB.Wx.Controllers
+{
+    public class WxController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            string code = Request.QueryString["code"];
+            string state = Request.QueryString["state"];
+            string s = Request.QueryString["s"];
+            if (!true)
+            {
+                OAuthAccessTokenResult result = OAuthApi.GetAccessToken(tenPayV3Info.AppId, tenPayV3Info.AppSecret, code);
+                if (result.errcode == Senparc.Weixin.ReturnCode.请求成功)
+                {
+                    string openid = result.openid;
+                    UserService x_userService = new UserService();
+                    var item = x_userService.Get(a => a.OpenId == openid).FirstOrDefault();
+                    if (item != null)
+                    {
+                        if (item.IsValid == 1)
+                        {
+                            Authentication.Instance.SetAuth(item, true);
+                        }
+                        else
+                        {
+                            return Redirect("/");
+                        }
+                    }
+                    else  //获取用户的信息
+                    {
+                        OAuthUserInfo wxUser = OAuthApi.GetUserInfo(result.access_token, code);
+                        if (wxUser != null)
+                        {
+                            UserInfo uInfo = new UserInfo()
+                            {
+                                IsValid = 1,
+                                CreateTime = DateTime.Now,
+                                //Lastlogintime = DateTime.Now,
+                                NickName = wxUser.nickname,
+                                OpenId = wxUser.openid,
+                                HeadImage = wxUser.headimgurl
+                            };
+                            uInfo.ID = Convert.ToInt32(x_userService.Insert(uInfo));
+                            Authentication.Instance.SetAuth(uInfo, true);
+                        }
+                        else
+                        {
+                            return Redirect("/");
+                        }
+                    }
+                }
+            }
+            string url = "/";
+            if (!string.IsNullOrEmpty(s))
+            {
+                url = s;
+            }
+            return Redirect(url);
+        }
+    }
+}
